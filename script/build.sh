0f21de118734e4d8ccf674e571b098da949e2e60
Ali Mustakim <alim@forwardcomputers.com>
2019-05-11 22:09:17 -0400

    updates [skip ci]

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-curl --silent --location --url https://github.com/forwardcomputers/dotfiles/raw/master/README.md | sed -n '/| Repository/,$p' > README.md
+curl --silent --location --url https://github.com/forwardcomputers/unattend/raw/master/README.md | sed -n '/| Repository/,$p' > README.md
+curl --silent --location --url https://github.com/forwardcomputers/dotfiles/raw/master/README.md | sed -n '/| [![]/,$p' >> README.md
 curl --silent --location --url https://github.com/forwardcomputers/home-assistant/raw/master/README.md | sed -n '/| [![]/,$p' >> README.md
 #
 curl --silent --location --url https://github.com/forwardcomputers/dockerfiles/raw/master/README.md | sed -n '/| [![]/,$p' >> README.md
