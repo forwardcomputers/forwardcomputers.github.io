3e022822bb6a9c66699e6b4eb78e6cfb6d3c8ce9
Ali Mustakim <alim@forwardcomputers.com>
2023-04-16 16:19:01 -0400

    updates [skip ci]

--- script/build.sh
+++ script/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-curl --silent --location --url https://github.com/forwardcomputers/dotfiles/raw/main/README.md | sed -n '/| Repository/,$p' > README.md
-curl --silent --location --url https://github.com/forwardcomputers/home-assistant/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
-curl --silent --location --url https://github.com/forwardcomputers/pxe/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
+curl -sL https://github.com/forwardcomputers/dotfiles/raw/main/README.md | sed -n '/| Repository/,$p' > README.md
+curl -sL https://github.com/forwardcomputers/home-assistant/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
+curl -sL https://github.com/forwardcomputers/pxe/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
 #
-curl --silent --location --url https://github.com/forwardcomputers/chocolatey-packages/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
+curl -sL https://github.com/forwardcomputers/chocolatey-packages/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
 #
-curl --silent --location --url https://github.com/forwardcomputers/dockerfiles/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
+curl -sL https://github.com/forwardcomputers/dockerfiles/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
 #
 
