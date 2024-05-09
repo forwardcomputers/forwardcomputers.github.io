7fe24989cb4b7a598bff0436838948f33f159269
Ali Mustakim <alim@forwardcomputers.com>
2024-05-09 10:17:18 -0400

    updates [skip ci]

--- script/build.sh
+++ script/build.sh
@@ -4,8 +4,8 @@
 # curl -sL https://github.com/forwardcomputers/home-assistant/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
 # curl -sL https://github.com/forwardcomputers/pxe/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
 #
-# curl -sL https://github.com/forwardcomputers/chocolatey-packages/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
-curl -sL https://github.com/forwardcomputers/chocolatey-packages/raw/main/README.md | sed -n '/| Repository/,$p' > README.md
+# curl -sL https://github.com/forwardcomputers/chocolatey-packages/raw/main/README.md | sed -n '/| Repository/,$p' > README.md
+curl -sL https://github.com/forwardcomputers/scoop-packages/raw/main/README.md | sed -n '/| Repository/,$p' > README.md
 #
 curl -sL https://github.com/forwardcomputers/dockerfiles/raw/main/README.md | sed -n '/| [![]/,$p' >> README.md
 #
